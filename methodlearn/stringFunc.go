bafeae48af23b0ebb6867fa248f2e3d9f973a4cf
Muhammad Sidik <pasker.shinigami@gmail.com>
2020-03-09 08:17:54 +0700

    Belajar fungsi string GO

--- methodlearn/stringFunc.go
+++ methodlearn/stringFunc.go
@@ -8,4 +8,31 @@ import (
 func stringFunc()  {
 	var apa = strings.Contains("Apria Ningsih", "Ningsih")
 	fmt.Println(apa)
+
+	var isPrefix = strings.HasPrefix("Apria Ningsih", "Ap")
+	fmt.Println(isPrefix)
+
+	var isPrefix2 = strings.HasPrefix("Apria Ningsih", "Ni")
+	fmt.Println(isPrefix2)
+
+	var isSuffix = strings.HasSuffix("Apria Ningsih", "ih")
+	fmt.Println(isSuffix) //true
+
+	var isSuffix2 = strings.HasSuffix("Apria Ningsih", "si")
+	fmt.Println(isSuffix2) //false
+
+	var Count = strings.Count("Apria Ningsih", "i")
+	fmt.Println(Count)
+
+	var indexk = strings.Index("Apria Ningsih", "i")
+	fmt.Println(indexk)
+
+	var Split = strings.Split("Apria Ningsih", " ")
+	fmt.Println(Split)
+
+	var data = []string{"Tri", "Apria", "Ningsih"}
+	var joins = strings.Join(data, "-")
+	fmt.Println(joins)
+
+
 }
